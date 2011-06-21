ccd560c8e8013c47127e7e3fa839029d0b98d7d9
arthemus.moreira <arthemus.moreira@gmail.com>
2011-06-21 04:04:03 +0000

    Criação do projeto Base de Conhecimento Definitivo: JSF 2.0 Tomcat 7.0 Java EE 6

---
+++ BaseConhecimento/src/java/br/com/softland/dthelp/model/connection/ConexaoAgenda.java
@@ -0,0 +1,43 @@
+/*
+ * Documento     : ConexaoAgenda.java
+ * Criação     : 14/05/2011
+ * Autor         : arthemus
+ * Descrição   : Classe java para conexao com o banco Agenda.FDB (Firebird)
+ * Modificação : 20/06/2011
+ */
+package br.com.softland.dthelp.model.connection;
+
+import java.sql.*;
+
+public class ConexaoAgenda {
+
+    private static String driver = null;
+    private static String url = null;
+    private static String login = null;
+    private static String senha = null;
+
+    public static Connection getConnection() {
+
+        driver = "org.firebirdsql.jdbc.FBDriver";
+
+        //Arthemus - Casa
+        url = "jdbc:firebirdsql:arthemus/3060:C:/ArvoreDeTrabalho/Bancos/AGENDA.FDB";
+        login = "SYSDBA";
+        senha = "masterkey";
+
+        //Arthemus - Softland
+        //url = "jdbc:firebirdsql:192.168.1.120/3060:D:/Bancos/Agenda/AGENDA.FDB";
+        //login = "SYSDBA";
+        //senha = "buana";
+
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, login, senha);
+            return con;
+        } catch (ClassNotFoundException erro) {
+            return null;
+        } catch (SQLException erro) {
+            return null;
+        }
+    }
+}
---
+++ BaseConhecimento/src/java/test/br/com/devmedia/mycompleteswingapp/dao/CustomerDAO.java
@@ -0,0 +1,55 @@
+package test.br.com.devmedia.mycompleteswingapp.dao;
+
+import test.br.com.devmedia.mycompleteswingapp.entity.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class CustomerDAO extends GenericDAO{
+
+   // private static final long SerialVesionUID = 1L;
+
+    public int addCustomer(Customers ctm) throws SQLException{
+        ctm.setId(getNextId("CUSTOMERS"));
+        String query = "INSERT INTO APP.CUSTOMERS(ID, NAME, AGE, ADDRESS, TELEPHONE) VALUES(?,?,?,?, ?)";
+        executeCommand(query, ctm.getId(), ctm.getName(), ctm.getAge(), ctm.getAddrress(), ctm.getTelephone());
+        return ctm.getId();
+    }
+
+    public void removeCustomers(int idCustomers) throws SQLException{
+        executeCommand("DELETE FROM APP.CUSTOMERS WHERE ID = ?", idCustomers);
+    }
+
+    public void updateCustomers(Customers ctm) throws SQLException{
+        String query = "UPDATE APP.CUSTOMERS SET NAME = ?, AGE = ?, ADDRESS = ?, TELEPHONE = ? WHERE ID = ?";
+        executeCommand(query, ctm.getName(), ctm.getAge(), ctm.getAddrress(), ctm.getTelephone(), ctm.getId());
+    }
+
+    public Customers getCustomers(int IdCustomers) throws SQLException{
+        ResultSet rs = executeQuery("SELECT * FROM APP.CUSTOMERS WHERE ID=?", IdCustomers);
+        Customers ctm = populateCustomersInfo(rs);
+        rs.close();
+        return ctm;
+    }
+
+    public static Customers populateCustomersInfo(ResultSet rs) throws SQLException {
+        Customers toReturn = new Customers();
+        toReturn.setId(rs.getInt("ID"));
+        toReturn.setName(rs.getString("NAME"));
+        toReturn.setAge(rs.getInt("AGE"));
+        toReturn.setAddrress(rs.getString("ADDRESS"));
+        toReturn.setTelephone(rs.getString("TELEPHONE"));
+        return toReturn;
+    }
+
+    public List<Customers> getAllProducts() throws SQLException{
+        List<Customers> toReturn = new LinkedList<Customers>();
+        ResultSet rs = executeQuery("SELECT * FROM APP.CUSTOMERS");
+        while(rs.next()){
+            toReturn.add(populateCustomersInfo(rs));
+        }
+        return toReturn;
+    }
+}
