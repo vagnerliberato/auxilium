c8b8c532394200c18ffb44e1421e08ee826e8417
arthemus.moreira <arthemus.moreira@gmail.com>
2011-07-20 19:48:21 +0000

    manutenção

--- BaseConhecimento/src/java/br/com/softland/baseConhecimento/controller/SenhaController.java
+++ BaseConhecimento/src/java/br/com/softland/baseConhecimento/controller/SenhaController.java
@@ -2,9 +2,7 @@ package br.com.softland.baseConhecimento.controller;
 
 import br.com.softland.baseConhecimento.bean.AnalistaBean;
 import br.com.softland.baseConhecimento.model.dao.SenhaDAO;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 @ManagedBean(name = "senhaController")
